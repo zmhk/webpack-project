30f62e553e4d51a8fafe842635a5d86df8cd370a
nchuhk <nchuhk@sohu.com>
2018-04-25 18:18:18 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as _ from "lodash"
+
+console.log(_.join);
+function component() {
+    var element = document.createElement('div');
+
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
